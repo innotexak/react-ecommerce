b43004c99b973fb766b9fdfff11a21e84a5bcce4
innotexak <akuhinnocent2016@gmail.com>
2021-06-25 05:42:48 +0100

    new feaure

---
+++ src/pages/klarna.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Shop</Link>
+        </li>
+        <li>
+          <Link to="/">Klarna</Link>
+        </li>
+        <li>
+          <Link to="/">Shop</Link>
+        </li>
+        <li>
+          <Link to="/">Shop</Link>
+        </li>
+        <li>
+          <Link to="/">Shop</Link>
+        </li>
+        <li>
+          <Link to="/">Shop</Link>
+        </li>
+        <li>
+          <Link to="/">Shop</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
